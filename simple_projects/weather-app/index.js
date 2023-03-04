c276e457c317b30080cb65c3827eafeee1c9a4dd
Skyeng <skyeng@MacBook-Air-Admin-2.local>
2023-03-04 20:06:22 +0300

    [ASMR-1]: weather app

---
+++ simple_projects/weather-app/index.js
@@ -0,0 +1,83 @@
+const container = document.querySelector('.container')
+const search = document.querySelector('.search-box button')
+const weatherBox = document.querySelector('.weather-box')
+const weatherDetails = document.querySelector('.weather-details')
+const error404 = document.querySelector('.not-found')
+const error401 = document.querySelector('.not-authorized')
+
+search.addEventListener('click', () => {
+    const city = document.querySelector('.search-box input').value
+    const APIKey = '226f29c9d6b1aa7272b37ac59f02d4ce'
+
+    if (city === '') return
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)
+        .then(res => res.json())
+        .then(json => {
+
+            if (json.cod === 404) {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            if (json.cod === 401) {
+                container.style.height = '200px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error401.style.display = 'block';
+                error401.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+            error401.style.display = 'none';
+            error401.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+
+                default:
+                    image.src = '';
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+        })
+})
